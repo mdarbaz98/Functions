a376cea17ada498105a7bd840c494c1474b33c8e
Arbaaz Shaikh <89142196+mdarbaz98@users.noreply.github.com>
2022-04-12 15:21:56 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,3 +6,11 @@ function scrollSmoothTo(elementId) {
 //   element.scrollIntoView({ block: "center", behavior: "smooth" });
   // element.hide();
 }
+
+// random number in every second
+      setInterval(function(){   
+    console.log(Math.floor((Math.random()*100)+1)); 
+        var user = $('#randomUser')[0]
+        user.innerHTML= Math.floor((Math.random()*100)+1)
+        console.log(user)
+ }, 1000);
